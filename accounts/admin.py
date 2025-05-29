f1c5331850ff616e49ddeecc0c37cc2839787efa
SIRI-bit-tech <siritech001@gmail.com>
2025-05-29 03:16:21 +0100

    refactor: update CustomUserAdmin to display security question

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,13 +3,13 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, UserProfile
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'email_verified')
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'get_security_question_display')
     search_fields = ('email', 'username', 'first_name', 'last_name')
     readonly_fields = ('date_joined', 'last_login')
     fieldsets = (
         (None, {'fields': ('email', 'username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),
-        ('Verification', {'fields': ('email_verified', 'verification_code', 'verification_code_created_at')}),
+        ('Security', {'fields': ('security_question',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
@@ -20,6 +20,10 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
     ordering = ('email',)
+    
+    def get_security_question_display(self, obj):
+        return obj.get_security_question_display()
+    get_security_question_display.short_description = 'Security Question'
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'date_of_birth', 'created_at', 'updated_at')
